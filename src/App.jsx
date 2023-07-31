46fa05200b7cc9231b94e094344005d9f7883455
麻生大翔 <hiroto.asou@M443-sakai.local>
2023-07-31 14:33:15 +0900

    Routingの調整

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import './componets/reset.css';
-import Index from './componets/Index.jsx';
-import AboutPage from './componets/About.jsx';
-import WorkPage from './componets/Work.jsx';
-import ContactPage from './componets/Contact.jsx';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import "./componets/reset.css";
+import Index from "./componets/Index.jsx";
+import AboutPage from "./componets/About.jsx";
+import WorkPage from "./componets/Work.jsx";
+import ContactPage from "./componets/Contact.jsx";
+import { Router, Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
+    const location = useLocation();
+    const ghPagesUrl = "/aso-portfolio";
 
-  const location = useLocation();
-  const ghPagesUrl ="/aso-portfolio";
-
-  return (
-    <div className="app">
-          <Routes location={location} key={location.pathname}>
-            <Route path={ ghPagesUrl + '/'} element={<Index />} />
-            <Route path={ ghPagesUrl + '/about' } element={<AboutPage />} />
-            <Route path={ ghPagesUrl + '/work' } element={<WorkPage />} />
-            <Route path={ ghPagesUrl + '/contact' } element={<ContactPage />} />
-            <Route path='*' element={<Index />} />
-          </Routes>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="app">
+                <Routes location={location} key={location.pathname}>
+                    <Route path={ghPagesUrl + "/"} element={<Index />} />
+                    <Route path={ghPagesUrl + "/about"} element={<AboutPage />} />
+                    <Route path={ghPagesUrl + "/work"} element={<WorkPage />} />
+                    <Route path={ghPagesUrl + "/contact"} element={<ContactPage />} />
+                    <Route path="*" element={<Index />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
