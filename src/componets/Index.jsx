780b543ba6cff89367da031e52a83f88e9898f49
麻生大翔 <hiroto.asou@M443-sakai.local>
2023-08-07 14:56:14 +0900

    アンマウント時の処理を追加

--- src/componets/Index.jsx
+++ src/componets/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../componets/style.css";
 import { Link } from "react-router-dom";
 import * as THREE from "three";
@@ -7,7 +7,14 @@ import { AmbientLight, PointLight } from "three";
 import BlackLine from "../utils/blackLine";
 
 const Index = () => {
+    const [isUnmounted, setIsUnmounted] = useState(false);
+
     useEffect(() => {
+        // ローディング画面の処理
+        setTimeout(() => {
+            document.getElementById("loading_area").style.display = "none";
+        }, 8100);
+
         let innerWidth = window.innerWidth;
         let innerheight = window.innerHeight;
 
@@ -15,6 +22,16 @@ const Index = () => {
             width: innerWidth,
             height: innerheight,
         };
+
+        const cleanup = () => {
+            if (model) {
+              scene.remove(model);
+              model = null;
+              mixer.stopAllAction();
+            }
+            setIsUnmounted(true);
+          };
+
         const canvas = document.getElementById("canvas");
         let model;
 
@@ -22,7 +39,7 @@ const Index = () => {
         const scene = new THREE.Scene();
 
         // camera
-        const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
         if (innerWidth < 1024) {
             camera.position.set(0, 0.55, 2);
         } else {
@@ -32,8 +49,8 @@ const Index = () => {
         // renderer
         const renderer = new THREE.WebGLRenderer({
             canvas: canvas,
-            antialias: true,
-            alpha: true,
+            antialias: false,
+            // alpha: true,
         });
         renderer.setSize(sizes.width, sizes.height);
         renderer.setPixelRatio(window.devicePixelRatio);
@@ -42,23 +59,15 @@ const Index = () => {
         const gltfLoader = new GLTFLoader();
         let mixer;
 
-        const tick = () => {
-            renderer.render(scene, camera);
-            if (mixer) {
-                mixer.update(0.01);
-            }
-            requestAnimationFrame(tick);
-        };
-
         gltfLoader.load(
             "./models/scene.gltf",
             (gltf) => {
+                if (isUnmounted) return; // コンポーネントがアンマウントされていた場合は処理を中止
                 model = gltf.scene;
                 model.scale.set(0.28, 0.28, 0.28);
                 model.rotation.y = -Math.PI / 0.415;
                 model.rotation.x = -Math.PI / 0.485;
                 model.rotation.z = -Math.PI / 0.485;
-                optimizeModelRenderingSpeed(model);
                 scene.add(model);
 
                 mixer = new THREE.AnimationMixer(model);
@@ -69,9 +78,18 @@ const Index = () => {
                 });
             },
             () => {
-                    tick();
+                tick();
             }
         );
+
+        const tick = () => {
+            renderer.render(scene, camera);
+            if (mixer) {
+                mixer.update(0.01);
+            }
+            requestAnimationFrame(tick);
+            console.log(isUnmounted)
+        };
 
         // light
         const ambientLight = new AmbientLight(0xffffff, 3);
@@ -79,38 +97,45 @@ const Index = () => {
         const pointLight = new PointLight(0xffffff, 2, 100);
         scene.add(pointLight);
 
-        // モデルの描写速度を上げる最適化関数
-        const optimizeModelRenderingSpeed = (model) => {
-            model.traverse((child) => {
-                if (child.isMesh) {
-                    child.frustumCulled = false; // フラスタムカリングを無効化
-                }
-            });
-        };
+        return cleanup;
     }, []);
 
     return (
         <div>
-            <BlackLine></BlackLine>
-            <canvas id="canvas"></canvas>
-            <div id="video">
-                <div className="header-main">
-                    <div className="main-logo-box">
-                        <p className="main-logo">
-                            <img src={process.env.PUBLIC_URL + "/images/logo/toukalogo4.png"} alt="" />
-                        </p>
-                        <nav className="header-navigation">
-                            <ul className="header-list">
-                                <li className="header-list-parts partsSP">
-                                    <Link className="to_a target " to="/aso-portfolio/about">
-                                        <p>Click to myPage</p>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </nav>
+            <BlackLine>
+                <div id="loading_area">
+                    <div className="load">
+                        <p>A</p>
+                        <p>S</p>
+                        <p>O</p>
+                        <p>H</p>
+                        <p>I</p>
+                        <p>R</p>
+                        <p>O</p>
+                        <p>T</p>
+                        <p>O</p>
                     </div>
                 </div>
-            </div>
+                <canvas id="canvas"></canvas>
+                <div id="video">
+                    <div className="header-main">
+                        <div className="main-logo-box">
+                            <p className="main-logo">
+                                <img src={process.env.PUBLIC_URL + "/images/logo/toukalogo4.png"} alt="" />
+                            </p>
+                            <nav className="header-navigation">
+                                <ul className="header-list">
+                                    <li className="header-list-parts partsSP">
+                                        <Link className="to_a target " to="/aso-portfolio/about" onClick="cleanup()">
+                                            <p>Click to myPage</p>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </BlackLine>
         </div>
     );
 };
